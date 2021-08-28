56f2529ebc4ab2c9d96555b4fb7d618a8ed0fb99
arttiam <71148141+arttiam@users.noreply.github.com>
2021-08-28 20:28:28 +0500

    правка

--- src/js/slide.js
+++ src/js/slide.js
@@ -1,13 +1,8 @@
 let btnCloses = document.querySelectorAll(".onboards__btn-close");
-let slide = document.querySelector(".onboards__wrapper");
 let slides = document.querySelectorAll(".swiper-slide");
 let btnCloseNext = document.querySelector(".onboards__btn--close");
 let btnFirst = document.querySelector(".onboard__btn--first");
 let btnNext = document.querySelector(".onboards__btn--next");
-let btnPagination = document.querySelector(".onboards__buttons-nav");
-let freeClose = document.querySelector(".onboards");
-let activeSlide = document.querySelector(".onboard__slide-active");
-let slideList = document.querySelector(".onboards__swiper");
 let slideFirst = document.querySelector(".onboard--first");
 const onboards = document.querySelector('.onboards');
 
@@ -16,11 +11,6 @@ var onboardSwiper = new Swiper(".onboards__swiper", {
     centeredSlides: true,
     slidesPerView: 1,
     resistanceRatio: 0,
-    // breakpoints: {
-    //     768: {
-    //         width: 701
-    //     },
-    // },
     pagination: {
         el: '.onboards__buttons-nav',
         type: 'bullets',
@@ -74,15 +64,4 @@ onboardSwiper.on('slidePrevTransitionStart', function (e) {
         btnNext.classList.remove('onboards__hide');
     }
 });
-
-
-/*btnFirst.addEventListener("click", function() {
-    btnNext.classList.remove("onboards__hide");
-    btnPagination.classList.remove("onboards__hide");
-    var nextSlide = activeSlide.nextElementSibling;
-    activeSlide.classList.remove("onboard__slideActive");
-    nextSlide.classList.add("index-slide");
-    activeSlide = nextSlide;
-});*/
-
 
